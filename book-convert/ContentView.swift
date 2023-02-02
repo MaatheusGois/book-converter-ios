23fb144e74ab021fabe303d0b1a8aa03067b10f6
Gois <matheusgoislimasilva@gmail.com>
2023-02-02 10:44:43 -0300

    feat(): New Updates

--- book-convert/ContentView.swift
+++ book-convert/ContentView.swift
@@ -8,22 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    private let startPage = 10
+    private let endPage = 25
+
     @State var text = ""
+
     var body: some View {
-        ScrollView {
-            Text(text).padding(28)
-        }.onAppear {
-            self.load(page: 1)
+        NavigationView {
+            ScrollView {
+                Text(text).padding(28)
+            }.onAppear {
+                self.load(page: startPage)
+            }.onTapGesture {
+                UIPasteboard.general.string = self.text
+            }
+            .toolbar {
+                Button("Copy") {
+                    UIPasteboard.general.string = self.text
+                }
+            }
         }
     }
 
     func load(page: Int) {
         let image = PDFManager.load(named: "clean-code", page: page)
         VisionManager.readImage(image: image) { text in
-            DispatchQueue.main.async { self.text += text ?? "" }
-            if page < 6 {
-                load(page: page + 1)
+            guard let text else { return }
+            DispatchQueue.main.async {
+                self.text += text
             }
+
+            if page < endPage { load(page: page + 1) }
+            print("✅✅✅ PAGE DONE: \(page) ✅✅✅")
         }
     }
 }
